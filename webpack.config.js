3c9845902e148af9592286c74d1f2cda895f5815
nibilin33 <nibilin33@gmail.com>
2022-02-07 01:37:56 +0000

    add file

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,58 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+function getPages() {
+  const pages = path.resolve(__dirname, "src/pages");
+  const dirs = fs.readdirSync(pages);
+  let pageConfig = {};
+  let plugins = [];
+  dirs.forEach((name) => {
+    const stat = fs.lstatSync(path.join(pages, name));
+    if (stat.isDirectory()) {
+      pageConfig[name] = path.join(pages, `${name}/index.js`);
+      plugins.push(
+        new HtmlWebpackPlugin({
+          template: path.join(pages, `${name}/index.html`),
+          filename: `${name}.html`,
+          chunks: [name],
+          inject: 'body'
+        })
+      );
+    }
+  });
+  return {
+    entry: pageConfig,
+    plugins
+  };
+}
+const pageConfig = getPages();
 module.exports = {
-    entry: {
-        particles: './src/particles/index.js'
+  entry: pageConfig.entry,
+  output: {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, "docs"),
+    crossOriginLoading: 'anonymous'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'docs'),
     },
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/docs',
-      },
-      devServer: {
-        static: {
-          directory: path.join(__dirname, 'docs'),
-        },
-        compress: true,
-        port: 9000,
-      },
+    compress: true,
+    port: 9000,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  entry: pageConfig.entry,
+  output: {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, "docs"),
+    crossOriginLoading: 'anonymous'
+  },
+  plugins: pageConfig.plugins
 };
-  
-  
+
